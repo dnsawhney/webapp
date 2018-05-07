33b27307db0c5011c8e9b3f2d8ae6c16b4938abd
trekPlus <trek.plus.in@gmail.com>
2018-05-07 21:46:35 +0530

    Updated Local setup

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, jsonify
+from flask import render_template
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'Flask is running!'
+    return render_template("index.html")
 
 
 @app.route('/data')
@@ -15,4 +17,4 @@ def names():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
