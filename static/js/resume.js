9dfd962e32b2c55efa2a8ada421265cbd3e6ffff
dnsawhney <dnsawhney@icloud.com>
2018-05-12 22:23:58 +0530

    Fixed Js to scroll to top

--- static/js/resume.js
+++ static/js/resume.js
@@ -1,6 +1,12 @@
 (function($) {
   "use strict"; // Start of use strict
 
+  $(document).ready(function() {
+         $('html, body').animate({
+           'scrollTop':   $('#about').offset().top
+         }, 500);
+    });
+
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
